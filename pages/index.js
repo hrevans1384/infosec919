12a727278788e4539b7482114e0bddcfba46fbdd
Ray <46538887+hrevans1384@users.noreply.github.com>
2026-02-23 10:05:50 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,7 @@ export default function Home() {
         <Header title="InfoSec919" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
+          Hello World
         </p>
       </main>
 
